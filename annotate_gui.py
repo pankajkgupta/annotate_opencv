5eb63be20b68a81121333e9a6f292af584eeb7f6
Pankaj Gupta <beckman16@gmail.com>
2018-04-02 10:14:43 -0400

    first commit with working GUI

---
+++ annotate_gui.py
@@ -0,0 +1,164 @@
+import cv2, numpy as np
+import sys
+from time import sleep
+
+import annotate_tools
+import pandas as pd
+import colorsys
+
+from Tkinter import *
+import ttk
+import os
+import glob
+
+
+root_path = r"/media/clpsshare/pgupta/0f4db67a-4533-45ff-b2e3-86cef598973d/"
+
+
+def get_filenames():
+    path = root_path + "*.mp4"
+    return [os.path.basename(x) for x in sorted(glob.glob(path))]
+
+def onselect(evt):
+    # Note here that Tkinter passes an event object to onselect()
+    w = evt.widget
+    index = int(w.curselection()[0])
+    value = w.get(index)
+    print('You selected item %d: "%s"' % (index, value))
+    show_video(root_path+value)
+
+def show_video(v_path):
+    cv2.destroyAllWindows()
+
+    cv2.namedWindow(player_wname)
+    cv2.moveWindow(player_wname, 400, 150)
+    cv2.namedWindow(control_wname)
+    cv2.moveWindow(control_wname, 400, 50)
+
+    # video = sys.argv[1]
+    # video = '/media/clpsshare/pgupta/0f4db67a-4533-45ff-b2e3-86cef598973d/0f4db67a-4533-45ff-b2e3-86cef598973d_0000.mp4'
+    cap = cv2.VideoCapture(v_path)
+    basepath = os.path.split(v_path)
+    annot_file = basepath[0] + '/pose_' + basepath[1][:-4] + '.csv'
+    annots = pd.read_csv(annot_file)
+    drawjoints.init(drawjoints.annots, joints, annots, player_wname, playerwidth, playerheight, colorDict)
+    cv2.setMouseCallback(player_wname, drawjoints.dragcircle, drawjoints.annots)
+    controls = np.zeros((50, int(playerwidth)), np.uint8)
+    cv2.putText(controls, "W/w: Play, S/s: Stay, A/a: Prev, D/d: Next, E/e: Fast, Q/q: Slow, Esc: Exit", (40, 20),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, 255)
+    tots = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+    i = 0
+    cv2.createTrackbar('S', 'video', 0, int(tots) - 1, flick)
+    cv2.setTrackbarPos('S', 'video', 0)
+    cv2.createTrackbar('F', 'video', 1, 100, flick)
+    frame_rate = int(cap.get(cv2.CAP_PROP_FPS))
+    if frame_rate is None:
+        frame_rate = 30
+    cv2.setTrackbarPos('F', 'video', frame_rate)
+
+    def process(im):
+        return cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+
+    status = 'stay'
+    while True:
+        cv2.imshow("controls", controls)
+        try:
+            if i == tots - 1:
+                i = 0
+            cap.set(cv2.CAP_PROP_POS_FRAMES, i)
+            ret, im = cap.read()
+            r = playerwidth / im.shape[1]
+            dim = (int(playerwidth), int(im.shape[0] * r))
+            im = cv2.resize(im, dim, interpolation=cv2.INTER_AREA)
+            if im.shape[0] > 600:
+                im = cv2.resize(im, (500, 500))
+                controls = cv2.resize(controls, (im.shape[1], 25))
+            # cv2.putText(im, status, )
+            cv2.imshow('video', im)
+
+            drawjoints.updateAnnots(drawjoints.annots, i, im)
+            status = {ord('s'): 'stay', ord('S'): 'stay',
+                      ord('w'): 'play', ord('W'): 'play',
+                      ord('a'): 'prev_frame', ord('A'): 'prev_frame',
+                      ord('d'): 'next_frame', ord('D'): 'next_frame',
+                      ord('q'): 'slow', ord('Q'): 'slow',
+                      ord('e'): 'fast', ord('E'): 'fast',
+                      ord('c'): 'snap', ord('C'): 'snap',
+                      -1: status,
+                      27: 'exit'}[cv2.waitKey(10)]
+
+            if status == 'play':
+                frame_rate = cv2.getTrackbarPos('F', 'video')
+                sleep((0.1 - frame_rate / 1000.0) ** 21021)
+                i += 1
+                cv2.setTrackbarPos('S', 'video', i)
+                continue
+            if status == 'stay':
+                i = cv2.getTrackbarPos('S', 'video')
+            if status == 'exit':
+                annots.to_csv(annot_file, index=False)
+                break
+            if status == 'prev_frame':
+                i -= 1
+                cv2.setTrackbarPos('S', 'video', i)
+                status = 'stay'
+            if status == 'next_frame':
+                i += 1
+                cv2.setTrackbarPos('S', 'video', i)
+                status = 'stay'
+            if status == 'slow':
+                frame_rate = max(frame_rate - 5, 0)
+                cv2.setTrackbarPos('F', 'video', frame_rate)
+                status = 'play'
+            if status == 'fast':
+                frame_rate = min(100, frame_rate + 5)
+                cv2.setTrackbarPos('F', 'video', frame_rate)
+                status = 'play'
+            if status == 'snap':
+                cv2.imwrite("./" + "Snap_" + str(i) + ".jpg", im)
+                print "Snap of Frame", i, "Taken!"
+                status = 'stay'
+
+        except KeyError:
+            print "Invalid Key was pressed"
+    cv2.destroyWindow('video')
+    cv2.destroyWindow('controls')
+
+# Define the drag object
+
+playerwidth = 320.0
+playerheight = 240.0
+
+def flick(x):
+    pass
+
+player_wname = 'video'
+control_wname = 'controls'
+
+
+
+joints = ['nose', 'r_shoulder', 'r_elbow', 'r_wrist', 'l_shoulder', 'l_elbow', 'l_wrist']
+
+
+NUM_COLORS = len(joints)
+colorList = [[0, 0, 255], [0, 255, 0], [0, 213, 0], [0, 170, 0], [255, 0, 0], [213, 0, 0], [170, 0, 0]]
+colorDict = dict(zip(joints, colorList))
+
+root = Tk()
+l = Listbox(root, selectmode = SINGLE, height=30, width=40)
+l.grid(column=0, row=0, sticky=(N,W,E,S))
+s = ttk.Scrollbar(root, orient=VERTICAL, command=l.yview)
+s.grid(column=1, row=0, sticky=(N,S))
+l['yscrollcommand'] = s.set
+ttk.Sizegrip().grid(column=1, row=1, sticky=(S,E))
+root.grid_columnconfigure(0, weight=1)
+root.grid_rowconfigure(0, weight=1)
+root.geometry('350x500+50+50')
+root.title('Select Video')
+for filename in get_filenames():
+    l.insert(END, filename)
+
+l.bind('<<ListboxSelect>>', onselect)
+
+root.mainloop()
+
---
+++ annotate_tools.py
@@ -0,0 +1,263 @@
+import cv2
+
+class Rect:
+    x = None
+    y = None
+    w = None
+    h = None
+
+    def printit(self):
+        print str(self.x) + ',' + str(self.y) + ',' + str(self.w) + ',' + str(self.h)
+        
+class Circle:
+    x = None
+    y = None
+    r = 8
+    # Drag in progress
+    drag = False
+    # already present
+    active = True
+    # Marker flags by positions
+    hold = False
+    label = ''
+
+    def __init__(self, label):
+        self.label = label
+
+    def printit(self):
+        print str(self.x) + ',' + str(self.y) + ',' + str(self.r)
+
+# endclass
+
+class annots:
+    # Limits on the canvas
+    keepWithin = Rect()
+
+    # To store rectangle anchor point
+    # Here the rect class object is used to store
+    # the distance in the x and y direction from
+    # the anchor point to the top-left and the bottom-right corner
+    anchor = {}
+    # Selection marker size
+    sBlk = 2
+    # Whether initialized or not
+    initialized = False
+
+    joints = {}
+
+    # Image
+    image = None
+
+    # Window Name
+    wname = ""
+
+    # Return flag
+    returnflag = False
+
+    frame_n = 0
+    joints_df = []
+    colorDict = {}
+    def __init__(self, label):
+        self.label = label
+        # To store circle
+        self.joints[label] = Circle(label)
+        self.anchor[label] = Circle(label)
+
+# endclass
+
+
+def init(annot_obj, joints, annots_df, windowName, windowWidth, windowHeight, colormap):
+    # Image
+    # annot_obj.image = Img
+
+    # Window name
+    annot_obj.wname = windowName
+    annot_obj.joints_df = annots_df
+    # Limit the selection box to the canvas
+    annot_obj.keepWithin.x = 0
+    annot_obj.keepWithin.y = 0
+    annot_obj.keepWithin.w = windowWidth
+    annot_obj.keepWithin.h = windowHeight
+
+    annot_obj.colorDict = colormap
+    frame_n = 0
+    for jt in joints:
+        annot_obj(jt)
+        # Set rect to zero width and height
+        annot_obj.joints[jt].x = 0
+        annot_obj.joints[jt].y = 0
+        annot_obj.joints[jt].r = 8
+        annot_obj.active = True
+
+
+# enddef
+
+def dragcircle(event, x, y, flags, dragObj):
+    if x < dragObj.keepWithin.x:
+        x = dragObj.keepWithin.x
+    # endif
+    if y < dragObj.keepWithin.y:
+        y = dragObj.keepWithin.y
+    # endif
+    if x > (dragObj.keepWithin.x + dragObj.keepWithin.w - 1):
+        x = dragObj.keepWithin.x + dragObj.keepWithin.w - 1
+    # endif
+    if y > (dragObj.keepWithin.y + dragObj.keepWithin.h - 1):
+        y = dragObj.keepWithin.y + dragObj.keepWithin.h - 1
+    # endif
+
+    jt = None
+    for joint_name in dragObj.joints:
+        joint = dragObj.joints[joint_name]
+
+        if joint.x == 'nan':
+            continue
+
+        if pointInCircle(x, y, int(joint.x), int(joint.y), joint.r):
+            jt = dragObj.joints[joint_name]
+            break
+
+    if jt is None:
+        return
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        mouseDown(x, y, dragObj, jt)
+    # endif
+    if event == cv2.EVENT_LBUTTONUP:
+        mouseUp(x, y, dragObj, jt)
+    # endif
+    if event == cv2.EVENT_MOUSEMOVE:
+        mouseMove(x, y, dragObj, jt)
+    # endif
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        mouseDoubleClick(x, y, dragObj, jt)
+    # endif
+
+# enddef
+
+# def pointInRect(pX, pY, rX, rY, rW, rH):
+def pointInCircle(pX, pY, rX, rY, rR):
+    if ((pX - rX)**2 ) + ((pY - rY)**2) < rR**2:
+    # if rX <= pX <= (rX + rW) and rY <= pY <= (rY + rH):
+        return True
+    else:
+        return False
+    # endelseif
+
+# enddef
+
+def updateAnnots(annots_obj, frame_n, im):
+
+    joints = annots_obj.joints.keys()
+    annot_df = annots_obj.joints_df[annots_obj.joints_df.frame_n == frame_n][joints]
+    if annot_df.empty:
+        return
+    # This has to be below all of the other conditions
+    # if pointInCircle(eX, eY, dragObj.outCircle.x, dragObj.outCircle.y, dragObj.outCircle.r):
+    annots_obj.image = im
+    annots_obj.frame_n = frame_n
+    for joint in annot_df:
+        annots_obj.joints[joint].x, annots_obj.joints[joint].y = annot_df[joint].values[0].split('-')
+
+    clearCanvasNDraw(annots_obj)
+    return
+
+def mouseDoubleClick(eX, eY, dragObj, jt):
+    if jt.active:
+
+        # if pointInCircle(eX, eY, dragObj.outCircle.x, dragObj.outCircle.y, dragObj.outCircle.r):
+        dragObj.returnflag = True
+        cv2.destroyWindow(dragObj.wname)
+        # endif
+
+    # endif
+
+
+# enddef
+
+def mouseDown(eX, eY, dragObj,jt):
+    if jt.active:
+
+        # This has to be below all of the other conditions
+        # if pointInCircle(eX, eY, dragObj.outCircle.x, dragObj.outCircle.y, dragObj.outCircle.r):
+        dragObj.anchor[jt.label].x = eX - int(jt.x)
+        dragObj.anchor[jt.label].y = eY - int(jt.y)
+        jt.hold = True
+        # jt.drag = True
+        return
+
+    else:
+        jt.x = eX
+        jt.y = eY
+        jt.drag = True
+        jt.active = True
+        return
+
+# enddef
+
+def mouseMove(eX, eY, dragObj, jt):
+    if jt.drag & dragObj.active:
+        clearCanvasNDraw(dragObj)
+        return
+    # endif
+
+    if jt.hold:
+        jt.x = eX - dragObj.anchor[jt.label].x
+        jt.y = eY - dragObj.anchor[jt.label].y
+
+        if jt.x < dragObj.keepWithin.x:
+            jt.x = dragObj.keepWithin.x
+        # endif
+        if jt.y < dragObj.keepWithin.y:
+            jt.y = dragObj.keepWithin.y
+        # endif
+        if (jt.x + jt.r) > (dragObj.keepWithin.x + dragObj.keepWithin.w - 1):
+            jt.x = dragObj.keepWithin.x + dragObj.keepWithin.w - 1 - jt.r
+        # endif
+        if (jt.y + jt.r) > (dragObj.keepWithin.y + dragObj.keepWithin.h - 1):
+            jt.y = dragObj.keepWithin.y + dragObj.keepWithin.h - 1 - jt.r
+        # endif
+
+        dragObj.joints_df.loc[dragObj.joints_df['frame_n'] == dragObj.frame_n, jt.label] = str(jt.x) + '-' + str(jt.y)
+        clearCanvasNDraw(dragObj)
+        return
+    # endif
+
+
+# enddef
+
+def mouseUp(eX, eY, dragObj, jt):
+    jt.drag = False
+    disableResizeButtons(dragObj)
+    jt.hold = False
+    jt.active =False
+
+    # endif
+
+    clearCanvasNDraw(dragObj)
+
+# enddef
+
+def disableResizeButtons(dragObj):
+    dragObj.hold = False
+
+
+# enddef
+
+def clearCanvasNDraw(dragObj):
+    # Draw
+    tmp = dragObj.image.copy()
+    tmp1 = dragObj.image.copy()
+    for joint_name in dragObj.joints:
+        joint = dragObj.joints[joint_name]
+        if joint.x == 'nan':
+            return
+        cv2.circle(tmp, (int(joint.x), int(joint.y)),
+               joint.r, dragObj.colorDict[joint_name], -1)
+    # apply the overlay
+    cv2.addWeighted(tmp, 0.5, tmp1, 0.5, 0, tmp1)
+    cv2.imshow(dragObj.wname, tmp1)
+    # cv2.waitKey()
+
+
+# enddef
